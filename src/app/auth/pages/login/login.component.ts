30633a3dd4cbcbcd85c3a63641de9bd71eeebdf1
Anderson Guedes <anderson.guedes@outlook.com.br>
2021-09-17 17:07:42 -0300

    Tela de login

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  signUpChange() {
+    const container = document.getElementById('container');
+    container?.classList.add('right-panel-active');
   }
 
+  signInChange() {
+    const container = document.getElementById('container');
+    container?.classList.remove('right-panel-active');
+  }
 }
