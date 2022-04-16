a1a427c35ceaf642e8d2e8b85b3ce1ab47907d0f
Anderson Guedes <anderson.guedes@outlook.com.br>
2022-04-16 08:26:41 -0300

    Ajuste na data no footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  currentYear: any;
+  initialYear: string = '2021';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.getCurrentYear();
   }
 
+  getCurrentYear() {
+    let currentTime = new Date();
+    this.currentYear = currentTime.getFullYear();
+  }
 }
